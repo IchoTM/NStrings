6a93de23898abb259ffbbf80fef68944ba60bf64
IchoDev <jms01192006@gmail.com>
2024-05-15 10:45:29 -0400

    Transition to .lib

--- nstring.h
+++ nstring.h
@@ -1,7 +1,15 @@
+/* * * * * * * * * * * * * * * * * * * * * * * * * * * 
+ *           EXPANDED STRINGS FOR C++                *
+ *                 BY: IchoTM                        *
+ * https://github.com/IchoTM/ExpandedStrings_for_CPP *
+ * * * * * * * * * * * * * * * * * * * * * * * * * * */
+#pragma once
+#define WIN32_LEAN_AND_MEAN
 #include <string>
 #include <iostream>
 #include <vector>
 using namespace std;
+
 class nstring : public string
 {
 	private:
@@ -15,7 +23,7 @@ class nstring : public string
 		void baseLaw();
 		void rmvLaw(char);
 		void getLaws() const;
-		vector<nstring> divideByDelim(char,bool);
+		vector<nstring> divideByDelim(char, bool);
 		nstring& operator=(const char[]);
 		nstring& operator=(nstring);
 		nstring& operator+=(char);
